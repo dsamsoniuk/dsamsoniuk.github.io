70cfe0a5ed506b9f992c70dc63bde8b7db318792
Damian <d.samsoniuk@protonmail.com>
2020-05-02 13:29:38 +0200

    burger menu

--- js/index.js
+++ js/index.js
@@ -12,4 +12,30 @@ $(document).ready(function(){
         workerMenu.changeTag(tag);
         workerContent.init(contentList);
     });
+    // Burger menu
+    $('#menu-switch').on('click', function(e){
+        e.preventDefault();
+        $('.menu-list-switch').toggle();
+    });
+    
+    $( window ).resize(function() {
+        var windowWidth = $(document).width(); 
+        if (windowWidth < 1140) {
+            $('#menu-switch').show();
+            $('.menu-list-switch').hide();
+        } else {
+            $('#menu-switch').hide();
+            $('.menu-list-switch').show();
+        }
+    });
+
+    var windowWidth = $(document).width(); 
+    if (windowWidth < 1140) {
+        $('#menu-switch').show();
+        $('.menu-list-switch').hide();
+    } else {
+        $('#menu-switch').hide();
+        $('.menu-list-switch').show();
+    }
+
 });
